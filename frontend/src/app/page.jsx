25f4c08dbccf01e1ae025b2d105bfcfb1e43ea03
Joseph Chun <joseph.chun@gmail.com>
2023-01-21 18:31:20 -0800

    add createpost function

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,99 +1,8 @@
 'use client';
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
-// import styles from './page.module.css'
-
-// const inter = Inter({ subsets: ['latin'] })
-
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>app/page.jsx</code>
-//         </p>
-//         <div>
-//           <a
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{' '}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className={styles.vercelLogo}
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className={styles.center}>
-//         <Image
-//           className={styles.logo}
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//         <div className={styles.thirteen}>
-//           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-//         </div>
-//       </div>
-
-//       <div className={styles.grid}>
-//         <a
-//           href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={inter.className}>
-//             Docs <span>-&gt;</span>
-//           </h2>
-//           <p className={inter.className}>
-//             Find in-depth information about Next.js features and API.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={inter.className}>
-//             Templates <span>-&gt;</span>
-//           </h2>
-//           <p className={inter.className}>Explore the Next.js 13 playground.</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={inter.className}>
-//             Deploy <span>-&gt;</span>
-//           </h2>
-//           <p className={inter.className}>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   )
-// }
 
 import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
-import { client, challenge, authenticate } from '../../api';
+import { client, challenge, authenticate, createPost } from '../../api';
 
 export default function Home() {
 	/* local state variables to hold user's address and access token */
@@ -150,6 +59,15 @@ export default function Home() {
 			console.log('Error signing in: ', err);
 		}
 	}
+	async function createStreamerPost() {
+		try {
+			const postData = await client.mutate({
+				mutation: createPost,
+			});
+		} catch (err) {
+			console.log('Error posting stream: ', err);
+		}
+	}
 
 	return (
 		<div>
@@ -163,6 +81,11 @@ export default function Home() {
 			)}
 			{/* once the user has authenticated, show them a success message */}
 			{address && token && <h2>Successfully signed in!</h2>}
+			{address && token && (
+				<div onClick={createStreamerPost}>
+					<button>Login</button>
+				</div>
+			)}
 		</div>
 	);
 }
