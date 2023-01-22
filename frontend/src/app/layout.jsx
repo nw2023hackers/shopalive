db2f09de222616c1469c81f875967a8ca7bfef7e
unko-chan <kliang30@my.bcit.ca>
2023-01-21 20:13:15 -0800

    Update layout.jsx

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
-
+import Nav
+ from "./components/Nav";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
+        <Nav />
         {children}
       </body>
     </html>
