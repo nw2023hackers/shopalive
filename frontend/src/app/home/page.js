7409e53fe9b4aa108cc1f8e496b5ca6ef59c39fc
unko-chan <kliang30@my.bcit.ca>
2023-01-21 23:38:57 -0800

    rename and remove code

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,20 +1,18 @@
 "use client";
-import Catagory from "../components/home/Catagory";
 import Filters from "../components/home/Filters";
 import Ad from "../components/home/Ad";
 import Live from "../components/home/Live";
 import TopTen from "../components/home/TopTen";
-import Video from "../components/Video";
+import CategoryList from "../components/home/CategoryList";
 
 export default function Page() {
   return (
     <div>
-      <Catagory />
+      <CategoryList />
       <Filters />
       <Ad />
       <Live />
       <TopTen />
-      {/* <Video /> */}
     </div>
   );
 }
--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Nav
  from "./components/Nav";
-import Category from "./components/home/Category";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -12,10 +11,7 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         <Nav />
-        {children}
-
-        <Category />
-        
+        {children}        
       </body>
     </html>
   );
