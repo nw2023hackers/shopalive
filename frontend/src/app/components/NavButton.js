fd1938243bf50e0a72f6405e0982ce077ee9ce22
unko-chan <kliang30@my.bcit.ca>
2023-01-21 20:13:54 -0800

    Update NavButton.js

--- frontend/src/app/components/NavButton.js
+++ frontend/src/app/components/NavButton.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
-    border: none;
-    color: white    ;
-    background-color: black;
-`
+  border: none;
+  color: white;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 4rem;
+  max-width: 4rem;
+  height: 6vh;
+  justify-content: space-evenly;
+`;
 
 function NavButton(props) {
-    //Button
-    //Text
-    //Icon
-    //onClick()
+  //Button
+  //Text
+  //Icon
+  //onClick()
   return (
     <div>
-        <Button>
-            {props.children}
-            {props.text}
-        </Button>
+      <Button>
+        {props.children}
+        {props.text}
+      </Button>
     </div>
-  )
+  );
 }
 
-export default NavButton
+export default NavButton;
