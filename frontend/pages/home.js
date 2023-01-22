783d6a904c7cca540b14c0039bae3c5c7da930a6
kirayoon <113317993+kirayoon@users.noreply.github.com>
2023-01-22 08:44:21 -0800

    Update home.js

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,40 +1,39 @@
-'use client';
-import Filters from '../components/home/Filters';
-import Ad from '../components/home/Ad';
-import Live from '../components/home/Live';
-import TopTen from '../components/home/TopTen';
-import CategoryList from '../components/home/CategoryList';
+"use client";
+import Filters from "../components/home/Filters";
+import Ad from "../components/home/Ad";
+import Live from "../components/home/Live";
+import TopTen from "../components/home/TopTen";
+import CategoryList from "../components/home/CategoryList";
 
 export default function Page() {
+  // const ws = new WebSocket("wss://ws.bitstamp.net");
 
-	const ws = new WebSocket("wss://ws.bitstamp.net");
+  //   const apiCall = {
+  //     event: "bts:subscribe",
+  //     data: { channel: "order_book_btcusd" },
+  //   };
 
-	const apiCall = {
-	  event: "bts:subscribe",
-	  data: { channel: "order_book_btcusd" },
-	};
-  
-	ws.onopen = (event) => {
-	  ws.send(JSON.stringify(apiCall));
-	};
-  
-	ws.onmessage = function (event) {
-	  const json = JSON.parse(event.data);
-	  try {
-		if ((json.event = "data")) {
-		  setBids(json.data.bids.slice(0, 5));
-		}
-	  } catch (err) {
-		console.log(err);
-	  }
-	};
-	return (
-		<div>
-			<CategoryList />
-			<Filters />
-			<Ad />
-			<Live />
-			<TopTen />
-		</div>
-	);
+  //   ws.onopen = (event) => {
+  //     ws.send(JSON.stringify(apiCall));
+  //   };
+
+  //   ws.onmessage = function (event) {
+  //     const json = JSON.parse(event.data);
+  //     try {
+  //       if ((json.event = "data")) {
+  //         setBids(json.data.bids.slice(0, 5));
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  return (
+    <div>
+      <CategoryList />
+      <Filters />
+      <Ad />
+      <Live />
+      <TopTen />
+    </div>
+  );
 }
